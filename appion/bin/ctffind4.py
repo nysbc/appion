912799d52acca856556abd8eb59bf88c14b9b674
John Pellman <john+github@libjpel.so>
2025-01-17 14:19:22 -0500

    hq command requires absolute path to expect script

--- appion/bin/ctffind4.py
+++ appion/bin/ctffind4.py
@@ -302,7 +302,7 @@ class ctfEstimateLoop(appionLoop2.AppionLoop):
 			f.write("expect eof")
 		os.chmod(expectscript, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
 
-		cmd = "hq --server-dir %s submit --cwd %s --wait --max-fails 3 --time-limit=5min --cpus 2 sudo -u %s /usr/bin/expect %s" % (os.getenv("HQ_SERVER_DIR","/common/etc/hq/ctffind4"), os.getenv("HQ_CWD", "/common/sw/hq/ctffind4/jobs/hq-current"), getpass.getuser(), expectscript)
+		cmd = "hq --server-dir %s submit --cwd %s --wait --max-fails 3 --time-limit=5min --cpus 2 sudo -u %s /usr/bin/expect %s" % (os.getenv("HQ_SERVER_DIR","/common/etc/hq/ctffind4"), os.getenv("HQ_CWD", "/common/sw/hq/ctffind4/jobs/hq-current"), getpass.getuser(), os.path.abspath(expectscript))
 		#Handles case where command fails because hq server has gone away.
 		success=False
 		while not success:
