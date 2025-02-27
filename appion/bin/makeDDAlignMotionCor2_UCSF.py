090f0b578f1b9be4107f5a9fba91921228beab96
John Pellman <john+github@libjpel.so>
2025-02-27 13:38:02 -0500

    turn off autoscaling for now

--- appion/bin/makeDDAlignMotionCor2_UCSF.py
+++ appion/bin/makeDDAlignMotionCor2_UCSF.py
@@ -8,44 +8,18 @@ from appionlib import apDatabase
 from appionlib import apDisplay
 from multiprocessing import Pool, Manager
 
-def main(numProcs,minProcs,maxProcs,q):
+def main():
 	makeStack = apDDMotionCorrMaker.MotionCor2UCSFAlignStackLoop()
-	return makeStack.run(True,numProcs,minProcs,maxProcs,q)
+	return makeStack.run()
 
 if __name__ == '__main__':
-	startPower=2
-	endPower=4
-	minProcs=2**startPower
-	maxProcs=2**endPower
-	notdone=True
-	imgCount=0
-	while notdone:
-		procs=2**startPower
-		for tmpPower in range(startPower+1,endPower+1):
-			if imgCount >= 2**tmpPower:
-				procs=2**tmpPower
-		p=Pool(procs)
-		m=Manager()
-		q=m.Queue(procs)
-		results=[]
-		apDisplay.printMsg("Starting Appion with %d parallel processes" % procs)
-		for _ in range(procs):
-			results.append(p.apply_async(main, (procs,minProcs,maxProcs, q)))
-		p.close()
-		p.join()
-		returnData=[r.get(1) for r in results]
-		returnData=[r for r in returnData if r]
-		try:
-			imgCounts=[int(r[0]) for r in returnData if r[0]]
-		except:
-			imgCounts=[]
-		notDones=[r[1] for r in returnData if r[1]]
-		if imgCounts:
-			imgCount=max(imgCounts)
-		else:
-			imgCount=0
-		if sum(notDones) == 0:
-			notdone=False
+	procs=16
+	p=Pool(procs)
+	apDisplay.printMsg("Starting Appion with %d parallel processes" % procs)
+	for _ in range(procs):
+		p.apply_async(main)
+	p.close()
+	p.join()
 
 		
 
