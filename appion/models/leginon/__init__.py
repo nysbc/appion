a9e627a8a5495473856b4223fe6731ac5821569a
John Pellman <john+github@libjpel.so>
2025-04-15 10:09:19 -0400

    Slight cleanup

--- appion/models/leginon/__init__.py
+++
@@ -1,6 +0,0 @@
-from .AcquisitionImageData import AcquisitionImageData
-from .CameraEMData import CameraEMData
-from .InstrumentData import InstrumentData
-from .PresetData import PresetData
-from .SessionData import SessionData
-from .UserData import UserData
