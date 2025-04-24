49fcc2623348d3c82c37dea9530bdbddae264c21
John Pellman <john+github@libjpel.so>
2025-04-24 17:31:12 -0400

    Fix to test script; set totaldose to correct value

--- test/test_motioncor.py
+++ test/test_motioncor.py
@@ -19,9 +19,9 @@ for imageid in validationData.keys():
         rendered_frame_size = int(validationData[imageid]["appionflags"]["rendered_frame_size"])
     else:
         rendered_frame_size = 1
-    if "doseweight" in validationData[imageid]["appionflags"].keys():
-        if validationData[imageid]["appionflags"]["doseweight"]:
-            totaldose = int(validationData[imageid]["appionflags"]["doseweight"])
+    if "totaldose" in validationData[imageid]["appionflags"].keys():
+        if validationData[imageid]["appionflags"]["totaldose"]:
+            totaldose = int(validationData[imageid]["appionflags"]["totaldose"])
         else:
             totaldose = 0.0
     else:
@@ -41,20 +41,15 @@ for imageid in validationData.keys():
     # elif "InEer" in kwargs.keys():
     #     print(validationData[imageid]["motioncorflags"]["InEer"])
     #     assert kwargs["InEer"] == validationData[imageid]["motioncorflags"]["InEer"]
-    #print(validationData[imageid]["motioncorflags"].keys())
-    #print(validationData[imageid]["appionflags"]["doseweight"])
-    try:
-        print("FmDose: Does %.3f == %s?" % (kwargs["FmDose"], validationData[imageid]["motioncorflags"]["FmDose"]))
-        assert "%.3f" % kwargs["FmDose"] == validationData[imageid]["motioncorflags"]["FmDose"]
-    except:
-        continue
-    #print("kV: Does %d == %s?" % (kwargs["kV"], validationData[imageid]["motioncorflags"]["kV"]))
-    #assert "%d" % kwargs["kV"] == validationData[imageid]["motioncorflags"]["kV"]
+    # print("FmDose: Does %.3f == %s?" % (kwargs["FmDose"], validationData[imageid]["motioncorflags"]["FmDose"]))
+    # assert "%.3f" % kwargs["FmDose"] == validationData[imageid]["motioncorflags"]["FmDose"]
+    # print("kV: Does %d == %s?" % (kwargs["kV"], validationData[imageid]["motioncorflags"]["kV"]))
+    # assert "%d" % kwargs["kV"] == validationData[imageid]["motioncorflags"]["kV"]
     #print("RotGain: Does %d == %s?" % (kwargs["RotGain"], validationData[imageid]["motioncorflags"]["RotGain"]))
     #assert "%d" % kwargs["RotGain"] == validationData[imageid]["motioncorflags"]["RotGain"]
     #print("FlipGain: Does %d == %s?" % (kwargs["FlipGain"], validationData[imageid]["motioncorflags"]["FlipGain"]))
     #assert "%d" % kwargs["FlipGain"] == validationData[imageid]["motioncorflags"]["FlipGain"]
-    #print("PixSize: Does %.3f == %s?" % (kwargs["PixSize"], validationData[imageid]["motioncorflags"]["PixSize"]))
-    #assert "%.3f" % kwargs["PixSize"] == validationData[imageid]["motioncorflags"]["PixSize"]
-    #print("Trunc: Does %d == %s?" % (kwargs["Trunc"], validationData[imageid]["motioncorflags"]["Trunc"]))
-    #assert "%.3f" % kwargs["Trunc"] == validationData[imageid]["motioncorflags"]["Trunc"]
+    print("PixSize: Does %.3f == %s?" % (kwargs["PixSize"], validationData[imageid]["motioncorflags"]["PixSize"]))
+    assert "%.3f" % kwargs["PixSize"] == validationData[imageid]["motioncorflags"]["PixSize"]
+    # print("Trunc: Does %d == %s?" % (kwargs["Trunc"], validationData[imageid]["motioncorflags"]["Trunc"]))
+    # assert "%.3f" % kwargs["Trunc"] == validationData[imageid]["motioncorflags"]["Trunc"]
