dd0ed7f7383201aecfc335dd16ed10a442a3d930
John Pellman <john+github@libjpel.so>
2025-05-06 15:14:42 -0400

    More refactoring of generic upload functions

--- appion/base/upload.py
+++ appion/base/upload.py
@@ -4,6 +4,7 @@ from sinedon.models.appion import ScriptHostName
 from sinedon.models.appion import ScriptProgramRun
 from sinedon.models.appion import ScriptParamName
 from sinedon.models.appion import ScriptParamValue
+from sinedon.models.appion import ApAssessmentRunData
 import platform
 import importlib.metadata
 import os, pwd
@@ -11,25 +12,26 @@ from cpuinfo import get_cpu_info
 import socket
 import distro
 import psutil
+from argparse import ArgumentParser
 
 
 def uploadScriptProgramName(name):
     progname = ScriptProgramName(name=name)
     progname.save()
 
-def uploadScriptUsername(name):
-    getpwnam=pwd.getpwnam(name)
-    username = ScriptUserName(name=name,
-                            uid = getpwnam[2],
-                            gid = getpwnam[3],
-                            fullname = getpwnam[4],
-                            unixshell = getpwnam[6])
+def uploadScriptUsername():
+    getpwuid=pwd.getpwuid(os.getuid())
+    username = ScriptUserName(name=getpwuid[0],
+                            uid = getpwuid[2],
+                            gid = getpwuid[3],
+                            fullname = getpwuid[4],
+                            unixshell = getpwuid[6])
     username.save()
 
 def uploadScriptHostName():
     cpu_info = get_cpu_info()
     hostname = ScriptHostName(name = platform.node(),
-							  ip = socket.gethostbyaddr(platform.node())[2][0],
+							  ip = socket.gethostbyaddr(platform.node())[2][0], # Not really accurate/useful since a host can have multiple IPs and we don't know which one we care about here.
 							  system = os.uname()[0].lower(),
 							  distro = distro.name(pretty=True),
 							  nproc = cpu_info["count"],
@@ -40,74 +42,90 @@ def uploadScriptHostName():
 							  )
     hostname.save()
 
-def uploadScriptProgramRun():
-    progrunq = appiondata.ScriptProgramRun()
-    progrunq['runname'] = self.params['runname']
-    progrunq['progname'] = prognameq
-    progrunq['username'] = userq
-    progrunq['hostname'] = hostq
-    progrunq['unixshell'] = unixshell
-    progrunq['rundir'] = appiondata.ApPathData(path=os.path.abspath(self.params['rundir']))
-    progrunq['job'] = self.getClusterJobData()
-    appiondir = apParam.getAppionDirectory()
-    ### get appion version/subversion revision
-    progrunq['revision'] = importlib.metadata.version('appion')
-    progrunq['appion_path'] = appiondata.ApPathData(path=os.path.abspath(appiondir))
+def uploadScriptProgramRun(runname, ref_scriptprogramname_progname, ref_scriptusername_username, ref_scripthostname_hostname, ref_appathdata_rundir, ref_apappionjobdata_job, ref_appathdata_appion_path):
+    getpwuid=pwd.getpwuid(os.getuid())
+    progrun = ScriptProgramRun(runname=runname,
+							   revision=importlib.metadata.version('appion'),
+							   ref_scriptprogramname_progname=ref_scriptprogramname_progname,
+							   ref_scriptusername_username=ref_scriptusername_username,
+							   ref_scripthostname_hostname=ref_scripthostname_hostname,
+							   ref_appathdata_rundir=ref_appathdata_rundir,
+							   ref_apappionjobdata_job=ref_apappionjobdata_job,
+							   ref_appathdata_appion_path=ref_appathdata_appion_path,
+							   unixshell=getpwuid[6]
+							   )
+    progrun.save()
 
-def uploadScriptParamName():
-    for paramname in self.params.keys():
-        paramnameq = appiondata.ScriptParamName()
-        paramnameq['name'] = paramname
-        paramnameq['progname'] = prognameq
+# Run parse_args() to get a namespace object and then transfrom that into a dict with vars()
+def uploadScriptParams(args : dict, ref_scriptprogramname_progname, ref_scriptprogramrun_progrun, parser : ArgumentParser):
+    for paramname in args.keys():
+        paramname = ScriptParamName(name=paramname,
+									ref_scriptprogramname_progname=ref_scriptprogramname_progname)
+        paramname.save()
+        usage=usageFromParamDest(args, parser, paramname, args[paramname])
+        if usage:
+            paramvalue = ScriptParamValue(value=str(args[paramname]),
+                                        usage = usage,
+                                        paramname=paramname.def_id,
+                                        ref_scriptprogramrun_progrun=ref_scriptprogramrun_progrun
+            )
+            paramvalue.save()
+			
+def usageFromParamDest(args, parser, dest, value):
+    """
+    For a given optparse destination (dest, e.g., 'commit')
+        and value (e.g., 'False') this will generate the command line
+        usage (e.g., '--no-commit')
+    """
+    usage = None
+    if value is None:
+        return None
+    if len(args) == 0:
+        for opt in parser.option_list:
+            arg = str(opt.get_opt_string.im_self)
+            if '/' in arg:
+                args = arg.split('/')
+                arg = args[-1:][0]
+                args[opt.dest] = arg
+                args[opt.dest] = opt
+        if dest in args:
+            argument=args[dest]
+        else:
+            return usage
+    if not dest in args:
+        return usage
+    optaction = args[dest].action
+    if optaction == 'store':
+        value = str(value)
+        if not ' ' in value:
+            usage = argument+"="+value
+        else:
+            usage = argument+"='"+value+"'"
+    elif optaction == 'store_true' or optaction == 'store_false':
+        storage = 'store_'+str(value).lower()
+        for opt in parser.option_list:
+            if opt.dest == dest and opt.action == storage:
+                arg = str(opt.get_opt_string.im_self)
+                if '/' in arg:
+                    arglist = arg.split('/')
+                    arg = arglist[-1:][0]
+                    usage = arg
+    return usage
 
-        paramvalueq = appiondata.ScriptParamValue()
-        paramvalueq['value'] = str(self.params[paramname])
-        usage = self.usageFromParamDest(paramname, self.params[paramname])
-        #print "usage: ", usage
-        paramvalueq['usage'] = usage
-        paramvalueq['paramname'] = paramnameq
-        paramvalueq['progrun'] = progrunq
-        if usage is not None:
-            paramvalueq.insert()
-				
 # ApAssessmentRunData
 
-def insertImgAssessmentStatus(imgdata, runname="run1", assessment=None, msg=True):
-	"""
-	Insert the assessment status
-		keep = True
-		reject = False
-		unassessed = None
-	"""
-	if assessment is True or assessment is False:
-		assessrun = appiondata.ApAssessmentRunData()
-		assessrun['session'] = imgdata['session']
-		#override to ALWAYS be 'run1'
-		#assessrun['name'] = runname
-		assessrun['name'] = "run1"
-
-		assessquery = appiondata.ApAssessmentData()
-		assessquery['image'] = imgdata
-		assessquery['assessmentrun'] = assessrun
-		assessquery['selectionkeep'] = assessment
-		assessquery.insert()
-	else:
-		apDisplay.printWarning("No image assessment made, invalid data: "+str(assessment))
-
-
-	#check assessment
-	if msg is True:
-		finalassess = getImgAssessmentStatus(imgdata)
-		imgname = apDisplay.short(imgdata['filename'])
-		if finalassess is True:
-			astr = apDisplay.colorString("keep", "green")
-		elif finalassess is False:
-			astr = apDisplay.colorString("reject", "red")
-		elif finalassess is None:
-			astr = apDisplay.colorString("none", "yellow")
-		apDisplay.printMsg("Final image assessment: "+astr+" ("+imgname+")")
-
-	return True
+def uploadApAssessmentRunData(ref_sessiondata_session, assessment="unassessed"):
+    """
+    Insert the assessment status
+        keep = True
+        reject = False
+        unassessed = None
+    """
+    if assessment != "unassessed":
+        # Run name is always run1.  Not sure what the rationale for this is.
+        assessrun = ApAssessmentRunData(ref_sessiondata_session=ref_sessiondata_session,
+                                        name = "run1")
+        assessrun.save()
 
 # ApAppionJobData
 
