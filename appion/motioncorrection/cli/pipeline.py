1259f6d088a16130236a80041296da8a6e2e043b
John Pellman <john+github@libjpel.so>
2025-06-26 12:56:30 -0400

    Have motioncor2 tasks request a GPU

--- appion/motioncorrection/cli/pipeline.py
+++ appion/motioncorrection/cli/pipeline.py
@@ -9,7 +9,7 @@ def pipeline(tasklist: list, args : dict, jobmetadata: dict, client : Client, re
         pretask_f=client.submit(preTask, imageid, args, pure=False, retries=retries)
         futures.append(pretask_f)
 
-        task_f=client.submit(lambda pretask_data : motioncor(**pretask_data[0]), pretask_f, pure=False, retries=retries)
+        task_f=client.submit(lambda pretask_data : motioncor(**pretask_data[0]), pretask_f, pure=False, retries=retries, resources={'GPU': 1})
         futures.append(task_f)
 
         posttask_f=client.submit(lambda pretask_data, task_data : postTask(imageid, pretask_data[0], pretask_data[1], jobmetadata, args, task_data[0]), pretask_f, task_f, pure=False, retries=retries)
