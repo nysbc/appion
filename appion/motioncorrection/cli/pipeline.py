7797f4ebe2ebd14c2a419424cdbf0672bba8941c
John Pellman <john+github@libjpel.so>
2025-06-26 16:02:03 -0400

    Name lambdas in pipeline so that we can provide estimated task durations to dask-jobqueue without mixing preTask and motioncor

--- appion/motioncorrection/cli/pipeline.py
+++ appion/motioncorrection/cli/pipeline.py
@@ -9,9 +9,11 @@ def pipeline(tasklist: list, args : dict, jobmetadata: dict, client : Client, re
         pretask_f=client.submit(preTask, imageid, args, pure=False, retries=retries, resources={'CPU': 1, "MEMORY" : 16})
         futures.append(pretask_f)
 
-        task_f=client.submit(lambda pretask_data : motioncor(**pretask_data[0]), pretask_f, pure=False, retries=retries, resources={'GPU': 1, "MEMORY" : 64})
+        motioncor_lambda = lambda pretask_data : motioncor(**pretask_data[0])
+        task_f=client.submit(motioncor_lambda, pretask_f, pure=False, retries=retries, resources={'GPU': 1, "MEMORY" : 64})
         futures.append(task_f)
 
-        posttask_f=client.submit(lambda pretask_data, task_data : postTask(imageid, pretask_data[0], pretask_data[1], jobmetadata, args, task_data[0]), pretask_f, task_f, pure=False, retries=retries, resources={'CPU': 1, "MEMORY" : 16})
+        postTask_lambda = lambda pretask_data, task_data : postTask(imageid, pretask_data[0], pretask_data[1], jobmetadata, args, task_data[0])
+        posttask_f=client.submit(postTask_lambda, pretask_f, task_f, pure=False, retries=retries, resources={'CPU': 1, "MEMORY" : 16})
         futures.append(posttask_f)
     return futures
