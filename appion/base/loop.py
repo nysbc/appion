61c533d330571b590efdb0710d72fcfd5faf5759
John Pellman <john+github@libjpel.so>
2025-06-27 15:55:41 -0400

    Change statistics that loop prints out.

--- appion/base/loop.py
+++ appion/base/loop.py
@@ -58,16 +58,26 @@ def loop(pipeline, args: dict, cluster : Cluster, retrieveDoneImages : Callable 
         logger.info("Constructed task list in %d seconds." % (t1-t0))
         logger.info("Image counts: %d total images, %d done images, %d rejected images, and %d images marked for reprocessing." % (len(all_images), len(done_images), len(rejected_images), len(reprocess_images)))
         if tasklist:
-            t0=time()
+            pipeline_t0=time()
             futures=pipeline(tasklist, args, jobmetadata, client)
             future_complete_counter=0
+            images_processed_t0=0
+            throughput_t0=time()
             for _ in as_completed(futures):
                 future_complete_counter+=1
                 if future_complete_counter % 100 == 0:
+                    throughput_t1=time()
                     done_images=retrieveDoneImages()
-                    logger.info("Image counts: %d total images, %d done images, %d rejected images, and %d images marked for reprocessing." % (len(all_images), len(done_images), len(rejected_images), len(reprocess_images)))
-            t1=time()
-            logger.info("Finished processing %d images in %d seconds." % (len(tasklist), (t1-t0)))
+                    images_processed_t1=len(done_images) - (len(all_images) - len(tasklist))
+                    throughput=((images_processed_t1-images_processed_t0)/(throughput_t1-throughput_t0))/60
+                    remaining_image_count=len(tasklist)-images_processed_t1
+                    logger.info("Progress: %d / %d images processed." % (images_processed_t1, len(tasklist)))
+                    logger.info("Throughput: %d images/min." % throughput)
+                    logger.info("Estimated remaining time: %d min." % remaining_image_count/throughput)
+                    throughput_t0=time()
+                    images_processed_t0=images_processed_t1
+            pipeline_t1=time()
+            logger.info("Finished processing %d images in %d seconds." % (len(tasklist), (pipeline_t1-pipeline_t0)))
         else:
             logger.info(f"No new images.  Waiting {waitTime} seconds.")
             sleep(waitTime)
