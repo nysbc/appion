69a649b6ef5d2193f96b3de6d51d3fc49fa645a1
John Pellman <john+github@libjpel.so>
2025-07-09 15:24:11 -0400

    updates to runJob.py

--- bin/runJob.py
+++ bin/runJob.py
@@ -6,15 +6,21 @@ from jinja2 import Environment, FileSystemLoader
 import os
 import subprocess
 import sys
-from argparse import ArgumentParser
+from shutil import which
+import copy
 
 def sbatch(batch_script_path : str):
     if os.path.exists(batch_script_path):
-        proc=subprocess.run("sbatch {batch_script_path}", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True, text=True, encoding="utf-8")
+        cmd=which("sbatch")
+        if not cmd:
+            return None
+        proc=subprocess.run([cmd, batch_script_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True, text=True, encoding="utf-8")
         return proc.stdout
     return None
 
 def renderJobScript(rundir: str, template_dir : str, template_file : str, command : str):
+    if not os.path.exists(rundir):
+        os.makedirs(rundir)
     batch_script_path = os.path.join(rundir, "batch.sh")
     with open(batch_script_path, "w") as jobFile:
         env = Environment(loader=FileSystemLoader(template_dir))
@@ -23,22 +29,20 @@ def renderJobScript(rundir: str, template_dir : str, template_file : str, comman
     return batch_script_path
 
 def main():
-    cmd=sys.argv[0]
-    # If there's a wrapper we need to remove the wrapper from the command name before rendering the command in the template.
-    # https://github.com/nysbc/leginon/blob/89218b54616e4f9e586b05c909708e199ea54106/myamiweb/processing/inc/processing.inc#L879-L883
-    # The simplest way to avoid this nonsense is to set USE_APPION_WRAPPER to false in myamiweb's config.php.
-    if "APPION_WRAPPER" in os.environ.keys():
-        appion_wrapper=os.environ["APPION_WRAPPER"]
-        cmd=appion_wrapper.split(cmd)
-        cmd=cmd[-1]
-        cmd=cmd.split()
-    else:
-        # Omit runJob.py
-        cmd=cmd[1:]
-    args=ArgumentParser(args=cmd)
+    cmd=copy.deepcopy(sys.argv)
+    rundir=""
+    while cmd:
+        cmd_token=cmd.pop(0)
+        if "=" in cmd_token:
+            cmd_token_split=cmd_token.split("=")
+            if len(cmd_token_split) > 1:
+                if cmd_token_split[0] in ["--rundir", "--outdir", "-R"]:
+                    rundir=cmd_token_split[1]
+    if not rundir:
+        raise RuntimeError("Could not find rundir argument")
     template_dir=os.environ.get("APPION_TEMPLATE_DIR","/etc/appion/templates")
     template_file=os.environ.get("APPION_JOB_TEMPLATE","slurm_job.sh.j2")
-    batch_script_path = renderJobScript(args.rundir, template_dir, template_file, " ".join(cmd))
+    batch_script_path = renderJobScript(rundir, template_dir, template_file, " ".join(sys.argv[1:]))
     stdout = sbatch(batch_script_path)
     print(stdout)
 
