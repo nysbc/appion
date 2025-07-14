7cb6e3147868ba056b48122643ddad31f8d700de
John Pellman <john+github@libjpel.so>
2025-07-14 16:13:07 -0400

    Pretend that motioncor2 functions are pure so that dask distributed doesn't try to run them twice.

--- appion/motioncorrection/cli/pipeline.py
+++ appion/motioncorrection/cli/pipeline.py
@@ -10,7 +10,7 @@ def pipeline(tasklist: list, args : dict, jobmetadata: dict, client : Client, re
     dask.config.set({"distributed.scheduler.unknown-task-duration":"30s"})
     futures=[]
     for imageid in tasklist:
-        pretask_f=client.submit(preTask, imageid, args, pure=False, retries=retries, resources={"MEMORY" : 16})
+        pretask_f=client.submit(preTask, imageid, args, pure=True, retries=retries, resources={"MEMORY" : 16})
         futures.append(pretask_f)
 
         # We give these lambdas names because Dask keeps track of function runtimes with a dict mapping
@@ -18,10 +18,10 @@ def pipeline(tasklist: list, args : dict, jobmetadata: dict, client : Client, re
         # See (State --> task_duration): https://docs.dask.org/en/latest/deploying-python-advanced.html#id2
         # and ('key' param) https://docs.dask.org/en/stable/futures.html#distributed.Client.submit
         motioncor_lambda = lambda pretask_data : motioncor(**pretask_data[0])
-        task_f=client.submit(motioncor_lambda, pretask_f, pure=False, retries=retries, resources={'GPU': 1, "MEMORY" : 64})
+        task_f=client.submit(motioncor_lambda, pretask_f, pure=True, retries=retries, resources={'GPU': 1, "MEMORY" : 64})
         futures.append(task_f)
 
         postTask_lambda = lambda pretask_data, task_data : postTask(imageid, pretask_data[0], pretask_data[1], jobmetadata, args, task_data[0], task_data[1])
-        posttask_f=client.submit(postTask_lambda, pretask_f, task_f, pure=False, retries=retries, resources={"MEMORY" : 16})
+        posttask_f=client.submit(postTask_lambda, pretask_f, task_f, pure=True, retries=retries, resources={"MEMORY" : 16})
         futures.append(posttask_f)
     return futures
