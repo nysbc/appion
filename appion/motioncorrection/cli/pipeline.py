1347059174f63e2515ccd0cd6977a4c42e239d85
John Pellman <john+github@libjpel.so>
2025-08-13 17:56:32 -0400

    Revert "Add GPU-locking and code to warm page cache so that we can put 2 dask workers on top of the same GPU"
    This reverts commit 7833cc34d0878b7694e44171efccb985a9558e6b.

--- appion/motioncorrection/cli/pipeline.py
+++ appion/motioncorrection/cli/pipeline.py
@@ -1,30 +1,8 @@
-import os
-from fcntl import flock, LOCK_EX, LOCK_UN
 from dask.distributed import Client
 import dask
 from .pretask import preTask
 from ..calc.external import motioncor, checkImageExists
 from .posttask import postTask
-
-def warmpagecache_motioncor(kwargs : dict):
-    ondisk_keys=["Gain", "Dark", "DefectMap", "FmIntFile", "InMrc", "InTiff", "InEer"]
-    for k in ondisk_keys:
-        if k in kwargs.keys():
-            if os.path.exists(kwargs[k]):
-                with open(kwargs[k], "rb") as f:
-                    f.read()
-
-def sliced_motioncor(kwargs : dict):
-    warmpagecache_motioncor(kwargs)
-    jobid=os.environ.get("SLURM_JOB_ID",None)
-    lockfile_path=os.path.join("/tmp", "%s.lock" % jobid)
-    if jobid:
-        with open(lockfile_path, "r") as f:
-            flock(f, LOCK_EX)
-            motioncor(**kwargs)
-            flock(f, LOCK_UN)
-    else:
-        raise RuntimeError("Could not determine Slurm job ID for motioncor time-slicing.")
 
 def pipeline(tasklist: list, args : dict, jobmetadata: dict, client : Client, retries : int = 0):
     # Sets the prior for tracking task duration to 30s so that adaptive scaling
@@ -40,7 +18,7 @@ def pipeline(tasklist: list, args : dict, jobmetadata: dict, client : Client, re
             # task keys to average durations.  Task keys == function names by default.
             # See (State --> task_duration): https://docs.dask.org/en/latest/deploying-python-advanced.html#id2
             # and ('key' param) https://docs.dask.org/en/stable/futures.html#distributed.Client.submit
-            motioncor_lambda = lambda pretask_data : sliced_motioncor(pretask_data[0])
+            motioncor_lambda = lambda pretask_data : motioncor(**pretask_data[0])
             task_f=client.submit(motioncor_lambda, pretask_f, pure=True, retries=retries, resources={'GPU': 1, "MEMORY" : 64})
             futures.append(task_f)
 
