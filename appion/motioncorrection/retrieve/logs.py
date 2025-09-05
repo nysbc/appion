e1e9a94a4cfbe43f29a384984aa2c206d950e6c5
John Pellman <john+github@libjpel.so>
2025-09-05 14:17:44 -0400

    Add function to split out logged messages by input path for a motioncor2 run that uses -Serial

--- appion/motioncorrection/retrieve/logs.py
+++ appion/motioncorrection/retrieve/logs.py
@@ -35,3 +35,32 @@ def parseMotionCorLog(outbuffer: list, shift_start: str) -> dict:
             raise RuntimeError("Could not parse shifts in log.  Line: %s" % line) from e
         line=outbuffer.pop(0)
     return logData
+
+def splitMotionCorLog(outbuffer: list) -> dict:
+    '''
+    Takes the output from a motioncor2/motioncor3 run that uses the Serial flag and splits out the log
+    outputs.
+    '''
+    log_split_buffer=[]
+    outbuffers={}
+    input_paths=[]
+    header=[]
+    footer=[]
+    while outbuffer:
+        line=outbuffer.pop(0)
+        if line.startswith("added:"):
+            input_paths.append(line.replace("added:", "").strip())
+        if "Load Tiff movie:" in line:
+            if not outbuffers and not header:
+                header=log_split_buffer
+            else:
+                outbuffers[input_paths.pop(0)]=log_split_buffer
+            log_split_buffer=[]
+        if "Total time:" in line:
+            outbuffers[input_paths.pop(0)]=log_split_buffer
+            log_split_buffer=[]
+            footer=log_split_buffer
+        log_split_buffer.append(line)
+    for k, v in outbuffers.items():
+        outbuffers[k]=header+[""]+v+[""]+footer
+    return outbuffers
