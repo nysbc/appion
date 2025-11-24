56d26bb21ccfec192df60beeb07583cc3dec1827
John Pellman <john+github@libjpel.so>
2025-11-24 15:15:41 -0500

    Switch to processing images in batch lists

--- appion/base/loop.py
+++ appion/base/loop.py
@@ -9,7 +9,7 @@ from .calc import filterImages
 from typing import Callable
 
 # Parameters passed in using lambdas.
-def loop(pipeline, args: dict, retrieveDoneImages : Callable = lambda : set(), preLoop : Callable = lambda args : {}, postLoop : Callable = lambda jobmetadata : None, retrieveReprocessImages : Callable = lambda : set(), min_workers : int = 0, max_workers : int = 32) -> None:
+def loop(pipeline, args: dict, retrieveDoneImages : Callable = lambda : set(), preLoop : Callable = lambda args : {}, postLoop : Callable = lambda jobmetadata : None, retrieveReprocessImages : Callable = lambda : set(), batch_size : int = 1, max_window : int = 200, parallelism : int = 32) -> None:
     jobmetadata={}
     # Signal handler used to ensure that cleanup happens if SIGINT, SIGCONT or SIGTERM is received.
     def handler(signum, frame):
@@ -42,12 +42,12 @@ def loop(pipeline, args: dict, retrieveDoneImages : Callable = lambda : set(), p
 
     jobmetadata=preLoop()
     waitTime=30
-    prev_tasklist=set()
+    prev_taskset=set()
     failure_cooldown=1
     failure_waits=0
 
     executor = submitit.AutoExecutor(folder=os.path.join(args["rundir"], "working"))
-    executor.update_parameters(timeout_min=6, slurm_partition="appion-motioncorrection", slurm_gres="gpu:1", slurm_cpus_per_task=2, slurm_array_parallelism=max_workers)
+    executor.update_parameters(timeout_min=6, slurm_partition="appion-motioncorrection", slurm_gres="gpu:1", slurm_cpus_per_task=2, slurm_array_parallelism=parallelism)
     while True:
         t0=time()
         all_images=readImageSet(args["sessionname"], args["preset"])
@@ -55,33 +55,41 @@ def loop(pipeline, args: dict, retrieveDoneImages : Callable = lambda : set(), p
         rejected_images=retrieveRejectedImages(all_images, args["sessionname"], None, None, args["tiltangle"])
         # Not used by motioncor2; used by ctffind4
         reprocess_images=retrieveReprocessImages()
-        tasklist=filterImages(all_images, done_images, reprocess_images, rejected_images)
-        unprocessed_image_count=len(tasklist)
-        if len(tasklist) > 200:
-            reduced_tasklist=list(tasklist)
-            reduced_tasklist.sort()
-            reduced_tasklist.reverse()
-            reduced_tasklist=reduced_tasklist[0:200]
-            tasklist=set(reduced_tasklist)
-        if prev_tasklist:
-            failureset=tasklist & prev_tasklist
-            tasklist=tasklist-failureset
+        taskset=filterImages(all_images, done_images, reprocess_images, rejected_images)
+        unprocessed_image_count=len(taskset)       
+        if prev_taskset:
+            failureset=taskset & prev_taskset
+            taskset=taskset-failureset
             logger.info("%d previously failed images will not be processed this iteration." % len(failureset))
+        tasklist=list(taskset)
+        tasklist.sort()
+        tasklist.reverse() 
         t1=time()
         logger.info("Constructed task list in %d seconds." % (t1-t0))
         logger.info("Image counts: %d total images, %d done images, %d rejected images, and %d images marked for reprocessing." % (len(all_images), len(done_images), len(rejected_images), len(reprocess_images)))
         if tasklist:
             pipeline_t0=time()
             futures=[]
+            batches=[]
+            taskStartIdx=0
+            taskEndIdx=batch_size
+            maxIdx=min(unprocessed_image_count, max_window)
+            for imageid in tasklist:
+                if taskEndIdx > maxIdx:
+                    taskEndIdx=maxIdx
+                batches.append(tasklist[taskStartIdx:taskEndIdx])
+                prev_taskset.add(tasklist[taskStartIdx:taskEndIdx])
+                taskStartIdx+=batch_size
+                taskEndIdx+=batch_size
             with executor.batch():
-                for imageid in tasklist:
-                    future = executor.submit(pipeline, imageid, args, jobmetadata)
+                for imageids in batches:
+                    future = executor.submit(pipeline, imageids, args, jobmetadata)
                     futures.append(future)
             future_complete_counter=0
             throughput_t0=time()
             future_complete_counter=0
             step=20
-            while future_complete_counter != len(futures):
+            while future_complete_counter != len(futures) and len(futures) != 0:
                 future_complete_counter = sum(f.done() for f in futures)
                 if future_complete_counter > step and future_complete_counter != 0:
                     throughput_t1=time()
@@ -96,13 +104,12 @@ def loop(pipeline, args: dict, retrieveDoneImages : Callable = lambda : set(), p
                     step+=20
                 sleep(10)
             pipeline_t1=time()
-            logger.info("Finished processing %d images in %d seconds." % (len(tasklist), (pipeline_t1-pipeline_t0)))
-            prev_tasklist=tasklist
+            logger.info("Finished processing %d images in %d seconds." % (len(prev_taskset), (pipeline_t1-pipeline_t0)))
         else:
             logger.info(f"No new images.  Waiting {waitTime} seconds.")
             sleep(waitTime)
-            if prev_tasklist:
+            if prev_taskset:
                 failure_waits+=1
                 if failure_waits >= failure_cooldown:
-                    prev_tasklist=set()
+                    prev_taskset=set()
                     failure_waits=0
--- appion/motioncorrection/cli/pipeline.py
+++ appion/motioncorrection/cli/pipeline.py
@@ -1,5 +1,5 @@
 
-def pipeline(imageid: int, args : dict, jobmetadata: dict):
+def pipeline(imageids: int, args : dict, jobmetadata: dict):
     # Sinedon needs to be reimported and setup within the local scope of this function
     # because the function runs as a Dask task, which means that it is run in a forked process
     # that doesn't have Django initialized.
@@ -8,7 +8,8 @@ def pipeline(imageid: int, args : dict, jobmetadata: dict):
     from .pretask import preTask
     from ..calc.external import motioncor, checkImageExists
     from .posttask import postTask
-    if checkImageExists(imageid):
-        kwargs, imgmetadata=preTask(imageid, args)
-        logData, logStdOut=motioncor(**kwargs)
-        postTask(imageid, kwargs, imgmetadata, jobmetadata, args, logData, logStdOut)
+    for imageid in imageids:
+        if checkImageExists(imageid):
+            kwargs, imgmetadata=preTask(imageid, args)
+            logData, logStdOut=motioncor(**kwargs)
+            postTask(imageid, kwargs, imgmetadata, jobmetadata, args, logData, logStdOut)
