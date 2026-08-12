6af902212e4f6b5d673dcdb95641d96572c7c79d
John Pellman <5000591+jpellman@users.noreply.github.com>
2026-08-12 17:07:08 -0400

    Modify motion correction ingest to take in project/session IDs.

--- appion/motioncorrection/cli/parser.py
+++ appion/motioncorrection/cli/parser.py
@@ -5,10 +5,10 @@ def constructCSMotionCorParser():
     parser = argparse.ArgumentParser(add_help=False)
 
     # Inputs
-    parser.add_argument("-i", "--cs_import_dir", dest="cryosparc_import_dir",
-        help="Path to CryoSPARC imports directory.", required=True)
-    parser.add_argument("-m", "--cs_motioncorrection_dir", dest="cryosparc_motioncorrection_dir",
-        help="Path to CryoSPARC motion correction outputs directory.", required=True)
+    parser.add_argument("-p", "--cs_project", dest="cryosparc_project",
+        help="CryoSPARC project number (integer).", type=int, required=True)
+    parser.add_argument("-s", "--cs_session", dest="cryosparc_session",
+        help="CryoSPARC live session number (integer).", type=int, required=True)
     # Integer
     parser.add_argument("--alignlabel", dest="alignlabel", default='a',
         help="label to be appended to the presetname, e.g. --label=a gives ed-a as the aligned preset for preset ed", metavar="CHAR")
--- bin/importCryoSparcMotioncor.py
+++ bin/importCryoSparcMotioncor.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2002-2015 Scripps Research Institute, 2015-2025 New York Structural Biology Center
+# Copyright 2002-2015 Scripps Research Institute, 2015-2026 New York Structural Biology Center
 
 import argparse
 import os
+from glob import glob
+import json
 from fcntl import flock, LOCK_EX, LOCK_UN
 from appion.base.cli import constructGlobalParser
 from appion.motioncorrection.cli.parser import constructCSMotionCorParser
 import sinedon.setup
+
+def find_cslive_session_dir(cs_project, cs_session):
+    csd=""
+    p_jsons=glob("/h[123]/*/cryosparc/*/project.json")
+    for p_json in p_jsons:
+        try:
+            with open(p_json,"r") as f:
+                j=json.load(f)
+            if "uid_num" in j.keys():
+                uid_num=int(j["uid_num"])
+            elif "uid" in j.keys():
+                uid_num=int(j["uid"].strip("P"))
+            else:
+                continue
+            if uid_num == cs_project:
+                csd=os.path.join(os.path.dirname(p_json),f"S{cs_session}")
+                if os.path.exists(csd):
+                    break
+        except:
+            continue
+    return csd
+
 
 def main():
     parser = argparse.ArgumentParser(parents=[constructGlobalParser(), constructCSMotionCorParser()])
@@ -26,6 +50,11 @@ def main():
     if not os.path.exists(lockfile):
         f=open(lockfile, "w")
         f.close()
+    cslive_session_dir=find_cslive_session_dir(args.cs_project, args.cs_session)
+    if not cslive_session_dir:
+        raise RuntimeError(f"No session directory found for P{args.cs_project}, S{args.cs_session}")
+    cryosparc_import_dir=os.path.join(cslive_session_dir, "import_movies")
+    cryosparc_motioncorrection_dir(cslive_session_dir, "motioncorrected")
     with open(lockfile, 'r+') as f:
         flock(f, LOCK_EX)
         f.seek(0)
@@ -35,9 +64,9 @@ def main():
         arg_dict=vars(args)
         if "FLOAT16_IMAGE" in os.environ.keys():
             float16_image_path=os.environ["FLOAT16_IMAGE"]
-            p=lambda imageid : process_task(imageid, arg_dict, args.cryosparc_import_dir, args.cryosparc_motioncorrection_dir, float16_image_path)
+            p=lambda imageid : process_task(imageid, arg_dict, cryosparc_import_dir, cryosparc_motioncorrection_dir, float16_image_path)
         else:
-            p=lambda imageid : process_task(imageid, arg_dict, args.cryosparc_import_dir, args.cryosparc_motioncorrection_dir)
+            p=lambda imageid : process_task(imageid, arg_dict, cryosparc_import_dir, cryosparc_motioncorrection_dir)
         loop(p,
                 arg_dict,
                 lambda : retrieveDoneImages(args.rundir, session_metadata['session_id']),
